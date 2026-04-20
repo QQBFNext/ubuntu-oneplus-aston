5af74e785ddfdcf27bc029abb7447a13590367fb
Shandorman <98683030+jiganomegsdfdf@users.noreply.github.com>
2026-04-20 20:35:46 +0300

    move to beta 26.04

--- aston-rootfs_build.sh
+++ aston-rootfs_build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-VERSION="25.10"
 
 cd $2
 
@@ -8,8 +7,8 @@ mkfs.ext4 rootfs.img
 mkdir rootdir
 mount -o loop rootfs.img rootdir
 
-wget https://cdimage.ubuntu.com/ubuntu-base/releases/$VERSION/release/ubuntu-base-$VERSION-base-arm64.tar.gz
-tar xzvf ubuntu-base-$VERSION-base-arm64.tar.gz -C rootdir
+wget https://cdimage.ubuntu.com/ubuntu-base/releases/26.04/beta/ubuntu-base-26.04-beta-base-arm64.tar.gz
+tar xzvf ubuntu-base-26.04-beta-base-arm64.tar.gz -C rootdir
 
 mkdir -p rootdir/data/local/tmp
 mount --bind /dev rootdir/dev
